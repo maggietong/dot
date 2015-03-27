d7da36555a71c4549f25b568e094a8d2e0a8f2bf
Mike Burr <mburr@unintuitive.org>
2015-03-27 07:19:51 -0700

    unvitalness

---
+++ bin/mainline_sync.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -ue
+
+MODE="${1:-}"
+
+NFS=/ws/miburr-sjc/git/mainline
+LOCAL=~/git/mainline
+
+if [ ! -d "$NFS" ] ; then
+    >&2 echo "$NFS not present (not mounted?)"
+    exit 1
+fi
+
+if [ `readlink -f $NFS` == `readlink -f $LOCAL` ] ; then
+    >&2 echo `readlink -f $NFS` == `readlink -f $LOCAL`
+    >&2 echo "Is your home dir NFS mounted?"
+    exit 1
+fi
+
+CMD="rsync -xva --sparse --delete --specials"
+
+if [ "$MODE" == "tonfs" ] ; then
+    $CMD "$LOCAL"/ "$NFS"/
+elif [ "$MODE" == "fromnfs" ] ; then
+    $CMD "$NFS"/ "$LOCAL"/
+else
+    >&2 echo "usage: `basename $0` <tonfs|fromnfs>"
+    exit 1
+fi
